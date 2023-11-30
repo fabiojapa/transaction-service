32b25470355d8194084cf5a028f83e5c757b0704
Fabio Sakamoto <fabiojapa@gmail.com>
2023-11-30 09:39:01 -0300

    Springdoc open-api

--- src/main/java/com/saka/transaction/dto/AccountDto.java
+++ src/main/java/com/saka/transaction/dto/AccountDto.java
@@ -1,6 +1,7 @@
 package com.saka.transaction.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AccountDto {
 
+    @Schema(nullable = true, description = "DO NOT map to json object and allows null")
     @JsonProperty("account_id")
     private Long accountId;
 
--- src/main/java/com/saka/transaction/dto/TransactionDto.java
+++ src/main/java/com/saka/transaction/dto/TransactionDto.java
@@ -1,6 +1,7 @@
 package com.saka.transaction.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TransactionDto {
 
+  @Schema(nullable = true, description = "DO NOT map to json object and allows null")
   @JsonProperty("transaction_id")
   private Long transactionId;
 
@@ -23,6 +25,7 @@ public class TransactionDto {
   @JsonProperty("operation_type_id")
   private Integer operationTypeId;
 
+  @Schema(nullable = true, description = "DO NOT map to json object and allows null")
   private String description;
 
   @NotNull
